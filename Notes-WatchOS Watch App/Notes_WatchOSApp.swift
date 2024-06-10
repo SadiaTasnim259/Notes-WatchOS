8eb17b4810d71960adac21824ece106d911b3ac1
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-06-10 14:29:33 +0600

    Initial Commit

---
+++ Notes-WatchOS Watch App/Notes_WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_WatchOSApp.swift
+//  Notes-WatchOS Watch App
+//
+//  Created by Sadia on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes_WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
